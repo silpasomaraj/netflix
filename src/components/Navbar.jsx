05b0490f23aa5a7bd1121a5412d420109f8ee47f
silpasomaraj <silpasomaraj117@gmail.com>
2023-12-16 13:26:26 +0530

    netflix project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='nav'>
+        <img  className='nav_image'width={'160px'}
+        src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"  />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Row.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import instance from '../instance';
+import './Row.css'
+function Row({title,fetchUrl}) {
+    const base_url="https://image.tmdb.org/t/p/original"
+    const[allMovies,setAllMovies]=useState([])
+   
+    const fetchData =async()=>{
+       const response= await instance.get(fetchUrl);
+       const {data}=response
+       
+       setAllMovies(data.results)
+    }
+    useEffect(()=>{
+
+        fetchData();
+
+    }, []);
+    console.log(allMovies);
+  return (
+    <div className='row'>
+        <h2 style={{color:'white'}}>{title}</h2>
+        <div className='movie_row'>
+            {
+                allMovies?.map(item=>(
+                    <img src={`${base_url}${item.poster_path}`} alt="" className='movie' />
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Row
